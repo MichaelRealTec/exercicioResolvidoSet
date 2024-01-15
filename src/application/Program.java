b8c26cecbca402dd88f47d922d728261cec0d52b
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-01-15 16:40:21 -0300

    exercicio 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,56 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.time.Instant;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-import entities.LogEntry;
-
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		Set<Integer> a = new HashSet<>();
+		Set<Integer> b = new HashSet<>();
+		Set<Integer> c = new HashSet<>();
+		
+		System.out.print("How many students for course A? ");
+		int n = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			a.add(number);
+		}
+
+		System.out.print("How many students for course B? ");
+		n = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			b.add(number);
+		}
+
+		System.out.print("How many students for course C? ");
+		n = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			c.add(number);
+		}
+		
+		Set<Integer> total = new HashSet<>(a);
+		total.addAll(b);
+		total.addAll(c);
+
+		System.out.println("Total students: " + total.size());
+
+		sc.close();
+		
+		
+		
+		
+		
+		
 		// Faz a leitura apenas da quantidade de usuários válidos sem repetição
 		
+		/*
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Enter file full path: ");
@@ -42,7 +77,7 @@ public class Program {
 			System.out.println("Error: " + e.getMessage());
 		}
 		sc.close();
-
+		*/
 	}
 
 }
